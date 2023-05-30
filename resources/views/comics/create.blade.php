452e226cbc43a4500365137d6b7c3178345c2c37
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-30 17:30:29 +0200

    refactor code

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -27,7 +27,7 @@
                     <input type="text" class="form-control" id="series" name="series">
                 </div>
                 <div class="mb-3">
-                    <label for="sale_date" class="form-label">Data di uscita YY/MM/DD</label>
+                    <label for="sale_date" class="form-label">Data di uscita YYYY-MM-DD</label>
                     <input type="text" class="form-control" id="sale_date" name="sale_date">
                 </div>
                 <div class="mb-3">
